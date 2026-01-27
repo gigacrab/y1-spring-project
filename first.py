ee787d9f7c98c9441f58deefb1b0b22d80766199
gigacrab <tehwenliang@gmail.com>
2026-01-27 13:29:27 +0800

    new commit

--- first.py
+++ first.py
@@ -2,41 +2,27 @@ from gpiozero import Robot, Motor
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-# Pin definitions (BCM numbering)
-IN1 = 27
-IN2 = 22
-ENA = 18
+# pin definitions (BCM numbering)
+IN1 = "BCM27"
+IN2 = "BCM22"
+ENA = "BCM18"
 
-IN3 = 23
-IN4 = 24
-ENB = 19
+IN3 = "BCM23"
+IN4 = "BCM24"
+ENB = "BCM19"
 
-# Use pigpio for hardware PWM
+# use pigpio for hardware PWM backend
 factory = PiGPIOFactory()
 
-left_motor = Motor(
-    forward=IN1,
-    backward=IN2,
-    enable=ENA,
-    pwm=True,
-    pin_factory=factory
-)
-
-right_motor = Motor(
-    forward=IN3,
-    backward=IN4,
-    enable=ENB,
-    pwm=True,
-    pin_factory=factory
-)
-
 robot = Robot(
-    left=left_motor,
-    right=right_motor,
+    left=Motor(forward=IN3, backward=IN4, enable=ENB),
+    right=Motor(forward=IN1, backward=IN2, enable=ENA),
     pin_factory=factory
 )
 
-# Move forward at 50% speed
-robot.forward(0.5)
+def turn():
+    pass
+
+robot.forward(speed=0.5)
 sleep(1)
 robot.forward(0)
