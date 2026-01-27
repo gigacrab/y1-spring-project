ec7a8bd32a587a714743b1c61408c9186fb10703
gigacrab <tehwenliang@gmail.com>
2026-01-27 13:15:57 +0800

    new commit

--- first.py
+++ first.py
@@ -23,4 +23,5 @@ factory = PiGPIOFactory()
 robot = Robot(left=Motor(forward=IN1, backward=IN2, pwm=ENA, pin_factory=factory), right=Motor(forward=IN3, backward=IN4, pwm=ENB, pin_factory=factory), pin_factory=factory)
 
 # Robot will now use hardware-driven PWM for speed
-robot.forward(0.5) 
+while True:
+    robot.forward(0.5) 
