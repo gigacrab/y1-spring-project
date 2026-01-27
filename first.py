91abfbf1d189acb488e48c82379fb4caa5319438
gigacrab <tehwenliang@gmail.com>
2026-01-27 10:43:32 +0800

    new commit

--- first.py
+++ first.py
@@ -1,10 +1,10 @@
 from gpiozero import Robot
 from gpiozero import Motor
-#from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero.pins.pigpio import PiGPIOFactory
 
 # Use pigpio for hardware-based timing
-#factory = PiGPIOFactory()
-robot = Robot(left=Motor("GPIO13", "GPIO19"), right=Motor("GPIO12", "GPIO18"))#, pin_factory=factory)
+factory = PiGPIOFactory()
+robot = Robot(left=Motor("GPIO13", "GPIO19"), right=Motor("GPIO12", "GPIO18"), pin_factory=factory)
 
 # Robot will now use hardware-driven PWM for speed
 robot.forward(0.5) 
