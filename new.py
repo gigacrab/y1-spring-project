6688cad5420f0cc74ff9655f8c9979cf56293bfd
gigacrab <tehwenliang@gmail.com>
2026-01-28 09:40:29 +0800

    new commit 2

---
+++ new.py
@@ -0,0 +1,53 @@
+import pigpio
+import time
+import sys
+
+pwm_freq = 800
+
+# time to complete one rev at max speed
+T_360 = 1.5
+
+IN1, IN2, ENA = 27, 22, 18
+IN3, IN4, ENB = 23, 24, 19
+
+# __main__ is the script that was passed to execute
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        left_speed = float(sys.argv[1])
+        right_speed = float(sys.argv[2])
+        if len(sys.argv) == 4:
+            pwm_freq = int(sys.argv[3])
+    else:
+        raise Exception("Didn't input appropriate variables")
+
+pi = pigpio.pi()
+
+pi.set_mode(IN1, pigpio.OUTPUT)
+pi.set_mode(IN2, pigpio.OUTPUT)
+pi.set_mode(ENA, pigpio.OUTPUT)
+
+pi.set_mode(IN3, pigpio.OUTPUT)
+pi.set_mode(IN4, pigpio.OUTPUT)
+pi.set_mode(ENB, pigpio.OUTPUT)
+
+pi.set_PWM_frequency(ENA, pwm_freq)
+pi.set_PWM_frequency(ENB, pwm_freq)
+
+def move(a, b, f):
+    pi.write(IN1, a > 0)
+    pi.write(IN2, a < 0)
+    pi.write(IN3, b > 0)
+    pi.write(IN4, b < 0)
+    pi.set_PWM_dutycycle(ENA, int(abs(a) * 255))
+    pi.set_PWM_dutycycle(ENB, int(abs(b) * 255))
+        
+def turn(speed, angle, dir):
+    turn_time = (1 / speed) * T_360 * (angle / 360)
+    if (dir.lower() == "left"):
+        move(-left_speed, -right_speed, pwm_freq)
+    else:   
+        move(left_speed, -right_speed, pwm_freq)
+    time.sleep(turn_time)
+    move(0, 0, pwm_freq)
+
+move(left_speed, right_speed, pwm_freq)
